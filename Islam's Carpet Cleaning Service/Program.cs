54a72b6cb524b97d7efed3ce267acaa063385adc
Keroles Hanna <keroleshannamatta@gmail.com>
2025-07-03 23:11:18 +0300

    Add project files.

---
+++ Islam's Carpet Cleaning Service/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Islam_s_Carpet_Cleaning_Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
